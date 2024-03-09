9f282710aefef103b1cd7e2e0f3a18221a947a8e
varunpbardwaj <varunbardwajp@gmail.com>
2024-03-09 17:58:07 +0530

    add author logo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -22,7 +22,7 @@ export default function Home() {
           <Image
             width="0"
             height="0"
-            className="w-20 h-20 sm:w-24 sm:h-24 md:w-32 md:h-32 contrast-125 brightness-75 rounded-full "
+            className="w-20 h-20 sm:w-24 sm:h-24 md:w-32 md:h-32 contrast-125 brightness-75 rounded-full mb-8"
             src="/varun.jpeg"
             alt="varun"
             unoptimized
