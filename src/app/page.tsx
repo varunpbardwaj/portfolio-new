ccaefe06e0a1b10c1ae1556317420556790b370a
varunpbardwaj <varunbardwajp@gmail.com>
2024-03-25 11:46:30 +0530

    change avatar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -22,8 +22,8 @@ export default function Home() {
           <Image
             width="0"
             height="0"
-            className="w-20 h-20 sm:w-24 sm:h-24 md:w-32 md:h-32 contrast-125 brightness-75 rounded-full mb-8"
-            src="/varun.jpeg"
+            className="w-20 h-20 sm:w-24 sm:h-24 md:w-32 md:h-32 contrast-125 brightness-75 rounded-3xl mb-8"
+            src="/varun.jpg"
             alt="varun"
             unoptimized
           />
